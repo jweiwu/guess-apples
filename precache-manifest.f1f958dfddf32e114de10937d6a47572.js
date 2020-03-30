86f51eff4ef04e542015a28350391d9ffe8c50f8
Wade <wade30191@gmail.com>
2020-03-30 16:44:39 +0800

    Updates

---
+++ precache-manifest.f1f958dfddf32e114de10937d6a47572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310bfc9402849bb243d52b28b41464f9",
+    "url": "/guess-apples/index.html"
+  },
+  {
+    "revision": "81168ba2ae746951df6b",
+    "url": "/guess-apples/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8e75f3c4ad865b1675ce",
+    "url": "/guess-apples/static/css/main.84589ed8.chunk.css"
+  },
+  {
+    "revision": "81168ba2ae746951df6b",
+    "url": "/guess-apples/static/js/2.7c162cf5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/guess-apples/static/js/2.7c162cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e75f3c4ad865b1675ce",
+    "url": "/guess-apples/static/js/main.f768754b.chunk.js"
+  },
+  {
+    "revision": "2e81c4c69dfcb64178d5",
+    "url": "/guess-apples/static/js/runtime-main.a313d151.js"
+  }
+]);
